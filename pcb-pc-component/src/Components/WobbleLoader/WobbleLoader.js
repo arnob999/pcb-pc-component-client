77bb22347e4f9ba3a13a13d6bc6eb91c6ef8d2e2
arnob999 <arnob196@gmail.com>
2023-03-27 00:50:24 +0600

    all components added

---
+++ pcb-pc-component/src/Components/WobbleLoader/WobbleLoader.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./WobbleLoader.css";
+const WobbleLoader = () => {
+  return (
+    <div>
+      <div className="wobbling-3 mx-auto"></div>
+    </div>
+  );
+};
+
+export default WobbleLoader;
