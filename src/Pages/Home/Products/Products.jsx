3eae7f89fd55e752abd4099c439803223afe07fa
Rohan1279 <muniruddinrohan@gmail.com>
2023-06-15 17:12:45 +0600

    search box functions done

--- src/Pages/Home/Products/Products.jsx
+++ src/Pages/Home/Products/Products.jsx
@@ -1,24 +1,39 @@
-import { useContext, useState } from "react";
-import { useLoaderData } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { useLoaderData, useLocation } from "react-router-dom";
 import { Authcontext } from "../../../contexts/AuthProvider";
 import ProductCard from "../../../Components/ProductCard";
 import BookingModal from "../../../Components/BookingModal";
+import { useQuery } from "@tanstack/react-query";
 
 export default function Products() {
-  const products = useLoaderData();
-  console.log(products);
+  // const allProducts = useLoaderData();
+  // console.log(products);
   const [product, setProduct] = useState(null);
   const { user } = useContext(Authcontext);
+  const { state } = useLocation();
+  console.log(state);
+  const {
+    data: queryProducts = [],
+    refetch,
+    isLoading,
+    isFetching,
+  } = useQuery({
+    queryKey: ["queryProducts"],
+    queryFn: () =>
+      fetch(`${process.env.REACT_APP_URL}/products?search=${state ?? ""}`).then(
+        (res) => res.json()
+      ),
+  });
+  useEffect(() => {
+    refetch();
+  }, [state]);
+  console.log(queryProducts);
   return (
     <div className="px-16">
       <h3 className="text-3xl text-center">
-        Products available of{" "}
-        <span className="underline decoration-blue-300">
-          {products[0].category_name}
-        </span>
-        : {products.length}
+        Products available : {queryProducts?.length}
         <div className="grid gap-10 my-10">
-          {products.map((product) => (
+          {queryProducts?.map((product) => (
             <ProductCard
               key={product._id}
               product={product}
